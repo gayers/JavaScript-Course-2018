d07370b399e6dea37d9e0d2c2684f338b87d07a4
Gia Ayers <gianayers@gmail.com>
2018-09-05 16:26:30 -0500

    Arrow Functions: Lexical 'this' keyword

--- 7-ES6/starter/script.js
+++ 7-ES6/starter/script.js
@@ -65,7 +65,7 @@ console.log(c);
 */
 
 
-// LECTURE: STRINGS
+/*// LECTURE: STRINGS
 
 let firstName = 'John';
 let lastName = 'Smith';
@@ -88,7 +88,78 @@ const n = `${firstName} ${lastName}`;
 console.log(n.startsWith('g'));
 console.log(n.endsWith('S'));
 console.log(n.includes(' '));
-console.log(`${firstName} `.repeat(5));
+console.log(`${firstName} `.repeat(5));*/
+
+// LECTURE: ARROW FUNCTION
+
+/*const years = [1991, 1965, 1982, 1937];
+
+// ES5
+var ages5 = years.map(function(el){
+    return 2016 - el;
+});
+
+console.log(ages5);
+
+
+// ES6  
+let ages6 = years.map(el => 2016 - el);
+console.log(ages6);
+
+ages6 = years.map((el, index) => `Age element ${index + 1}: ${2016 - el}.`);
+console.log(ages6);
+
+ages6 = years.map((el, index) =>{
+    const now = new Date().getFullYear();
+    const age = now - el;
+    return `Age element ${index + 1}: ${age}.`
+});
+
+console.log(ages6);*/
+
+
+// LECTURE: ARROW FUNCTIONS 2
+
+// ES5  
+var box5 = {
+    color: 'green',
+    position: 1,
+    clickMe: function(){
+        
+        var self = this;
+        document.querySelector('.green').addEventListener('click', function(){
+            var str = 'This is box number ' + self.position + ' and it is ' + self.color;
+            alert(str);
+        });
+    }
+}
+
+box5.clickMe();
+
+
+// ES6
+const box6 = {
+    color: 'green',
+    position: 1,
+    clickMe: function(){
+
+        document.querySelector('.green').addEventListener('click', () => {
+            var str = 'This is box number ' + this.position + ' and it is ' + this.color;
+            alert(str);
+        });
+    }
+}
+
+box6.clickMe();
+
+
+
+
+
+
+
+
+
 
 
 
